33726b704d261d9f3bc0b1fda949aff7867739e1
Yuewen Yang <100012430+kapa-moon@users.noreply.github.com>
2025-05-22 21:08:18 -0400

    tiptap type

--- branch-for-writing/src/types/tiptap.ts
+++ branch-for-writing/src/types/tiptap.ts
@@ -1,9 +1,15 @@
+export interface TiptapMark {
+  type: string;
+  attrs?: Record<string, any>; // Tiptap's internal type uses 'any' here
+  [key: string]: any; // To match Tiptap's expectation of { [key: string]: any; ... }
+}
+
 export interface TiptapNode {
   type: string;
-  attrs?: Record<string, any>;
+  attrs?: Record<string, unknown>;
   content?: TiptapNode[];
   text?: string;
-  marks?: any[]; // You can define a more specific mark type if needed
+  marks?: TiptapMark[];
 }
 
 export interface TiptapDocument {
